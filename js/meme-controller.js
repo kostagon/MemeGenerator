a8e7414d8611db366938dbea43a517e3e48634fc
kostagon <49278316+kostagon@users.noreply.github.com>
2019-10-23 18:54:38 +0300

    after phase 3

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,17 +1,28 @@
 'use strict';
 let gCanvas = document.querySelector('.main-canvas');
 let gCtx = gCanvas.getContext('2d');
-let gCurrMeme = getCurrMeme();
 
 function initCanvas() {
+    let currMeme = getCurrMeme();
     let image = new Image();
-    image.src = `./images/meme-images/${gCurrMeme.selectedImgId}.jpg`;
-
+    image.src = `./images/meme-images/${currMeme.selectedImgId}.jpg`;
+    let currMemeTxt = currMeme.txts[currMeme.selectedTxtIdx].line;
+    document.querySelector('#text').value = currMemeTxt;
     image.onload = () => {
         gCanvas.width = image.width;
         gCanvas.height = image.height;
-        renderImage(image);
+        renderImage(image, currMeme);
     }
+}
+
+function onManipulateText(val) {
+    let type = val.dataset.type;
+    doManipulateText(type);
+}
+
+function onManipulateLines() {
+    let val = document.querySelector('#text').value;
+    doManipulateLines(val);
 }
 
 function onChangeImage(val) {
@@ -19,15 +30,27 @@ function onChangeImage(val) {
     doChangeImage(imageId);
 }
 
-function renderImage(image) {
+function renderImage(image, currMeme) {
+    let selectedTxtIdx = currMeme.selectedTxtIdx;
+
     gCtx.drawImage(image, 0, 0);
-    gCtx.font = gCurrMeme.txts[0].size + 'px impact';
-    gCtx.fillStyle = '#fff';
-    gCtx.strokeStyle = '#000';
-    gCtx.fillText(gCurrMeme.txts[0].line, 10, 50);
-    gCtx.strokeText(gCurrMeme.txts[0].line, 10, 50);
-    gCtx.fill();
-    gCtx.stroke();
+    currMeme.txts.forEach((item, idx) => {
+        let pos = item.pos;
+
+        gCtx.font = item.size + 'px impact';
+        gCtx.fillStyle = '#fff';
+        gCtx.strokeStyle = '#000';
+        if (!idx) {
+            gCtx.fillText(item.line, 10, pos);
+            gCtx.strokeText(item.line, 10, pos);
+        }
+        if (idx) {
+            gCtx.fillText(item.line, 10, pos);
+            gCtx.strokeText(item.line, 10, pos);
+        }
+        gCtx.fill();
+        gCtx.stroke();
+    })
 }
 
 function onChangeText(newTxt) {
--- js/meme-service.js
+++ js/meme-service.js
@@ -3,22 +3,48 @@
 let gMeme = {
     selectedImgId: 1,
     selectedTxtIdx: 0,
-    txts: [
-        {
-            line: '',
-            size: 60,
+    txts: [{
+            line: 'Top Text',
+            size: 50,
             align: 'left',
-            color: 'red'
+            color: 'red',
+            pos: 50
+        },
+        {
+            line: 'Bottom Text',
+            size: 50,
+            align: 'left',
+            color: 'red',
+            pos: 450
         }
-    ]
+    ],
 }
+
+function doManipulateText(val) {
+    if (val === 'increase') gMeme.txts[gMeme.selectedTxtIdx].size++;
+    else if (val === 'decrease') gMeme.txts[gMeme.selectedTxtIdx].size--;
+    else if (val === 'move-up') gMeme.txts[gMeme.selectedTxtIdx].pos--;
+    else if (val === 'move-down') gMeme.txts[gMeme.selectedTxtIdx].pos++;
+    initCanvas();
+}
+
+function doManipulateLines(newVal) {
+    gMeme.txts[gMeme.selectedTxtIdx].line = newVal;
+    if (!gMeme.selectedTxtIdx) {
+        gMeme.selectedTxtIdx = 1
+    } else {
+        gMeme.selectedTxtIdx = 0;
+    }
+    initCanvas();
+}
+
 function doChangeImage(imageId) {
     gMeme.selectedImgId = imageId;
     initCanvas();
 }
 
 function doChangeText(newTxt) {
-    gMeme.txts[0].line = newTxt;
+    gMeme.txts[gMeme.selectedTxtIdx].line = newTxt;
     initCanvas();
 }
 
